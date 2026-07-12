a04be18e0e422fe796d537e35d229c751d4bd234
Christopher Francisco <cjfg00gl3@gmail.com>
2026-07-12 15:13:04 -0400

    UPDATE AuthManager to include auth_id and prompt for user ID input

--- src/auth_manager.rs
+++ src/auth_manager.rs
@@ -1,13 +1,32 @@
+use std::io::{Write};
+
 pub struct AuthManager
 {
-
+    pub user_id:String
 }
 
 impl AuthManager
 {
     pub fn new()-> Self 
     {    
-        println!("Go to your Spotify profile and your unique username will be at the end of the loaded url.");
-        Self {}
+        println!("Load your Spotify profile, your unique id will be in the URL.");
+        Self 
+        {
+            user_id: String::new()
+        }
+    }
+
+    pub fn prompt_id()-> Self
+    {
+        print!("Enter your userid: https://open.spotify.com/user/");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Failed to read");
+        println!("You've provided the following unique ID: {input}");
+        
+        Self{
+            user_id: input.trim().to_string()
+        }
     }
 }
