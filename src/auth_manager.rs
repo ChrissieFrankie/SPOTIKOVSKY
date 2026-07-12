878e0a5305c38717d85e31fb0856d23db1345ce1
Christopher Francisco <cjfg00gl3@gmail.com>
2026-07-12 14:25:20 -0400

    CREATE AuthManager

---
+++ src/auth_manager.rs
@@ -0,0 +1,13 @@
+pub struct AuthManager
+{
+
+}
+
+impl AuthManager
+{
+    pub fn new()-> Self 
+    {    
+        println!("Go to your Spotify profile and your unique username will be at the end of the loaded url.");
+        Self {}
+    }
+}
