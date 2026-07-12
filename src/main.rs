0f491ff77b17d88391b46d9514075753b5f3b647
Christopher Francisco <cjfg00gl3@gmail.com>
2026-07-12 15:57:28 -0400

    UPDATE main.rs to instantiate AuthManager and retrieve user ID for display

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ fn print_welcome() {
 
 fn main() { 
     print_welcome();
-    AuthManager::new();
-    AuthManager::prompt_user_id();
+    let my_auth_manager = AuthManager::new();
+    let my_auth_manager = AuthManager::prompt_user_id(my_auth_manager);
+    let user_id = AuthManager::get_user_id(my_auth_manager);
+    print!("Your Spotify user id is {user_id}");
 }
