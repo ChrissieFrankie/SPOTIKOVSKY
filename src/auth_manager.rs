af1b5b3048319e0c92f3e9e2b80820f6e39150d2
Christopher Francisco <cjfg00gl3@gmail.com>
2026-07-12 15:56:13 -0400

    REFINE AuthManager structure and methods to enhance user ID handling and retrieval

--- src/auth_manager.rs
+++ src/auth_manager.rs
@@ -1,13 +1,13 @@
 use std::io::{Write};
 
-pub struct AuthManager
+pub struct AuthManager // structure
 {
     pub user_id:String
 }
 
-impl AuthManager
+impl AuthManager // implementation
 {
-    pub fn new()-> Self 
+    pub fn new()-> Self // provide instructions
     {    
         println!("Load your Spotify profile, your unique id will be in the URL.");
         Self 
@@ -16,15 +16,19 @@ impl AuthManager
         }
     }
 
-    pub fn prompt_user_id()-> Self
+    pub fn prompt_user_id(mut my_auth_manager: Self)-> AuthManager // prompt for user id
     {
         print!("Enter your profile URL:");
         std::io::stdout().flush().unwrap();
 
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).expect("Failed to read"); 
-        Self{
-            user_id: input.rsplit('/').last().unwrap_or("").to_string()
-        }
+        my_auth_manager.user_id = input.split('/').last().unwrap_or("").to_string();
+        my_auth_manager
+    }
+
+    pub fn get_user_id(my_auth_manager: Self)-> String // return user id
+    {
+        my_auth_manager.user_id
     }
 }
